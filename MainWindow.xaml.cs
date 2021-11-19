cbc8926d41a66c034e6d05181386b31a9f4dbe55
Alexey Artemyev <alexey.artem.alexey.artem@gmail.com>
2021-11-19 23:50:37 +0300

    Add user interface

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,19 @@ namespace DataClusterer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    enum TypeMethod 
+    {
+        KMeans,
+        KMeansPlusPlus,
+        CMeans
+    }
+
+    enum TypeMeasure 
+    {
+        EuclideanDistance
+    }
+
     public partial class MainWindow : Window
     {
         private Random _rand;
@@ -32,9 +45,11 @@ namespace DataClusterer
             InitializeComponent();
             _rand = new Random();
             _clusteringMethod = new KMeansPlusPlus(3, new EuclideanDistance());
+            cbMethodSelection.ItemsSource = Enum.GetValues(typeof(TypeMethod)).Cast<TypeMethod>();
+            cbMeasureSelection.ItemsSource = Enum.GetValues(typeof(TypeMeasure)).Cast<TypeMeasure>();
         }
 
-        private List<double[]> LoadCSVFile() 
+        private List<double[]> LoadCSVFile(out string nameFile) 
         {
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Title = "Выберите csv-файл данных для кластеризации";
@@ -42,8 +57,11 @@ namespace DataClusterer
             
             if (ofd.ShowDialog() == false)
             {
+                nameFile = "";
                 return null;
             }
+
+            nameFile = ofd.SafeFileName;
 
             var config = new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = false };
             List<double[]> data = new List<double[]>();
@@ -110,11 +128,68 @@ namespace DataClusterer
             return result;
         }
 
+        private void SetClusteringMethod() 
+        {
+            MeasureSimilarity measureSimilarity;
+
+            Enum.TryParse(cbMeasureSelection.SelectedItem.ToString(), out TypeMeasure typeMeasure);
+            switch (typeMeasure) 
+            {
+                case TypeMeasure.EuclideanDistance:
+                    measureSimilarity = new EuclideanDistance();
+                    break;
+                default:
+                    measureSimilarity = new EuclideanDistance();
+                    break;
+            }
+
+            Enum.TryParse(cbMethodSelection.SelectedItem.ToString(), out TypeMethod typeMethod);
+            int amountClusters = (int)udAmountClusters.Value;
+            switch (typeMethod)
+            {
+                case TypeMethod.KMeans:
+                    _clusteringMethod = new KMeans(amountClusters, measureSimilarity);
+                    break;
+                case TypeMethod.CMeans:
+                    _clusteringMethod = new CMeans(amountClusters, measureSimilarity);
+                    break;
+                case TypeMethod.KMeansPlusPlus:
+                    _clusteringMethod = new KMeansPlusPlus(amountClusters, measureSimilarity);
+                    break;
+            }
+        }
+
         private void btSelectionFile_Click(object sender, RoutedEventArgs e)
         {
-            _data = LoadCSVFile();
+            _data = LoadCSVFile(out string nameFile);
+            tbNameFile.Text = nameFile;
+        }
+
+        private void btExecuteClusterization_Click(object sender, RoutedEventArgs e)
+        {
+            if (_data == null) 
+            {
+                MessageBox.Show("Чтобы выполнить кластеризацию, выберите файл с данными", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            SetClusteringMethod();
             var result = _clusteringMethod.ExecuteClusterization(DataConverter.ReduceDemension(_data.ToArray(), 2));
             chart.Series = FillSeriesCollection(result.Clusters);
+        }
+
+        private void btRefreshColor_Click(object sender, RoutedEventArgs e)
+        {
+            SeriesCollection series = chart.Series;
+            if (series == null) return;
+
+            foreach (var s in series.Where(s => s.Title != "Centroid")) 
+            {
+                if (s is ScatterSeries scatter)
+                {
+                    scatter.Fill = GetRandomBrush();
+                }
+            }
         }
     }
 }
